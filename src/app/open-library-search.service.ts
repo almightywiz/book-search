e77f0e3e281e126127d3f7803dd852572ecac493
Charles Lukas <charles.lukas@gmail.com>
2019-02-21 03:26:20 -0600

    adding more docs

--- src/app/open-library-search.service.ts
+++ src/app/open-library-search.service.ts
@@ -7,9 +7,15 @@ import { OpenLibrarySearchResponse } from './open-library-search-response';
   providedIn: 'root'
 })
 export class OpenLibrarySearchService extends OpenLibraryService {
-  protected static readonly SEARCH_URL = OpenLibraryService.BASE_SERVICE_URL + '/search.json';
+  protected static readonly SEARCH_URL =
+    OpenLibraryService.BASE_SERVICE_URL + '/search.json';
 
-  public static readonly SUPPORTED_SEARCH = ['author', 'title', 'text', 'subject'];
+  public static readonly SUPPORTED_SEARCH = [
+    'author',
+    'title',
+    'text',
+    'subject'
+  ];
 
   private static readonly DEFAULT_SEARCH_CRIT = 'q';
   private static readonly SEARCH_TERM_TAG = 'tag';
@@ -30,21 +36,37 @@ export class OpenLibrarySearchService extends OpenLibraryService {
     const searchTerms = this.generateSearchQuery(terms);
     const lim = 'limit=0';
 
-    return this.http.get<OpenLibrarySearchResponse>(
-      OpenLibrarySearchService.SEARCH_URL + '?' + [lim, searchTerms].join('&')
-    ).toPromise().then(res => {
-      const NUM_FOUND = 'num_found';
-      const limit = 'limit=' + (res[NUM_FOUND] > 1000 ? 1000 : res[NUM_FOUND]);
+    /*
+      Perform 2 requests; the first to determine the total number of documents
+      exist for the current criteria, and the second to retrieve all data.
 
-      if (res[NUM_FOUND] > 1000) {
-        console.debug('Search criteria is too broad, resulting in ' + res[NUM_FOUND]
-          + ' records.  Only the first 1000 records will be viewable.');
-      }
+      It should be noted that if the total number of records exceeds 1000,
+      then the limit will be capped at 1000.
+     */
+    return this.http
+      .get<OpenLibrarySearchResponse>(
+        OpenLibrarySearchService.SEARCH_URL + '?' + [lim, searchTerms].join('&')
+      )
+      .toPromise()
+      .then(res => {
+        const NUM_FOUND = 'num_found';
+        const limit =
+          'limit=' + (res[NUM_FOUND] > 1000 ? 1000 : res[NUM_FOUND]);
 
-      return this.http.get<OpenLibrarySearchResponse>(
-        OpenLibrarySearchService.SEARCH_URL + '?' + [limit, searchTerms].join('&')
-      );
-    });
+        if (res[NUM_FOUND] > 1000) {
+          console.debug(
+            'Search criteria is too broad, resulting in ' +
+              res[NUM_FOUND] +
+              ' records.  Only the first 1000 records will be viewable.'
+          );
+        }
+
+        return this.http.get<OpenLibrarySearchResponse>(
+          OpenLibrarySearchService.SEARCH_URL +
+            '?' +
+            [limit, searchTerms].join('&')
+        );
+      });
   }
 
   generateSearchQuery(terms: object[]): string {
@@ -57,19 +79,18 @@ export class OpenLibrarySearchService extends OpenLibraryService {
   }
 
   buildSearchTerms(searchTerms: object, terms: object[]) {
-
     let searchTag: string;
     let searchTerm: string;
     for (const term of terms) {
       searchTag = term[OpenLibrarySearchService.SEARCH_TERM_TAG];
       searchTerm = term[OpenLibrarySearchService.SEARCH_TERM_TERM];
       if (OpenLibrarySearchService.SUPPORTED_SEARCH.includes(searchTag)) {
-        searchTerms[searchTag] += ' "' + searchTerm.toString() + '"';
+        searchTerms[searchTag] += ' '' + searchTerm.toString() + ''';
       } else if (searchTag == null) {
-        searchTerms[OpenLibrarySearchService.DEFAULT_SEARCH_CRIT] += ' "' + searchTerm.toString() + '"';
+        searchTerms[OpenLibrarySearchService.DEFAULT_SEARCH_CRIT] +=
+          ' '' + searchTerm.toString() + ''';
       }
     }
-
   }
 
   consolidateSearchTerms(searchTerms: object): string {
